dd77bae75d2befc60f96a4acb474fac13d249d47
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-05 23:45:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Console.WriteLine(2 < 4);
+
+//Working with operators ;
+// Console.WriteLine("Calculate the actual values of this simple arithmetic");
+//  var UserSolution = Console.ReadLine();
+// Console.WriteLine("The total answer is " + UserSolution);
+
+//Getting an input from the user;
+// Console.WriteLine("How old are you?");
+// var UserAnwser = Console.ReadLine();
+// Console.WriteLine($"You're, {UserAnwser} years old!");
---
+++ learning_path/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace learning_path;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Time = 1;
+        var Rate = 50.4F; 
+        var Money = 22000.0;
+
+        var TotalAmount = (Time * Rate * Money) / 100;
+        Console.WriteLine(TotalAmount);
+
+        //Concatinating strings
+        var fruitList = "Apples, Orange, Mango and Lemon";
+        Console.WriteLine("There are " + fruitList + " in the fruits list of food.");
+    }
+}
